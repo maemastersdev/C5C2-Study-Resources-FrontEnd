78f5f2d22e9206b44e87db35b7ab2372edb72fa8
Salman0112358 <Salman0112358@outlook.com>
2022-09-23 10:43:04 +0100

    styling and rendering resource information onto our card components

--- src/components/ResourceCard/ResourceCard.tsx
+++ src/components/ResourceCard/ResourceCard.tsx
@@ -1,50 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 /*------------STYLING IMPORT -----------------------*/
 import "./ResourceCard.scss";
 /*------------ASSET IMPORT-----------------------*/
 
-import images from '../../assets/images/images'
+import spacePictures from "../../assets/images/spacePictures";
+import {
+  IResourceArray,
+  ITagsArray,
+  IUserName,
+} from "../../Interfaces/Interfaces";
+import axios from "axios";
+import serverUrl from "../../utils/serverUrl";
+import getRandomProperty from "../../utils/getRandomPropery";
 
-const ResourceCard = (): JSX.Element => {
+const ResourceCard = ({
+  resource_id,
+  resource_name,
+  review,
+  url,
+  author_name,
+  thumbnail,
+  user_id,
+}: IResourceArray): JSX.Element => {
+  const [tagsList, setTagsList] = useState<ITagsArray[]>([]);
+  const [randomIndex, setRandomIndex] = useState("");
+  const [currentUserName, setCurrentUserName] = useState<IUserName>();
+
+  useEffect(() => {
+    const getTagsForResource = async (): Promise<void> => {
+      const response = await axios.get(`${serverUrl}/tags/${resource_id}`);
+      const tags: ITagsArray[] = await response.data;
+      setTagsList(tags);
+    };
+    const getSingleUserName = async (): Promise<void> => {
+      const response = await axios.get(`${serverUrl}/user/${user_id}`);
+      const nameResponse = await response.data;
+      setCurrentUserName(nameResponse);
+    };
+
+    getTagsForResource();
+    getSingleUserName();
+    setRandomIndex(getRandomProperty(spacePictures));
+  }, [resource_id, user_id]);
+
   return (
-    <>
-      <div className="container">
-        <div className="post">
-          <div className="header_post">
+    <div className="container">
+      <div className="post">
+        <div className="header_post">
+          <a href={url} rel="noreferrer" target="_blank">
             <img
-              src={images.SpaceOne}
-              alt=""
+              src={
+                thumbnail
+                  ? thumbnail
+                  : spacePictures[randomIndex as keyof typeof spacePictures]
+              }
+              alt="thumbnail"
             />
-          </div>
-
-          <div className="body_post">
-            <div className="post_content">
-              <h1>Title Goes HERE</h1>
-              <p>
-                Example Resource Description
+            <div>
+              <p className="user_name">
+                Posted by {currentUserName?.user_name}{" "}
               </p>
+            </div>
+          </a>
+        </div>
 
-              <div className="container_infos">
-                <div className="postedBy">
-                  <span>author</span>
-                  John Doe
-                </div>
+        <div className="body_post">
+          <div className="post_content">
+            <h1>{resource_name}</h1>
+            <p>{review}</p>
 
-                <div className="container_tags">
-                  <span>tags</span>
-                  <div className="tags">
-                    <ul>
-                      <li>design</li>
-                      <li>front end</li>
-                    </ul>
-                  </div>
+            <div className="container_infos">
+              <div className="postedBy">
+                <span>author</span>
+                {author_name}
+              </div>
+              <div className="container_tags">
+                <span>tags</span>
+                <div className="tags">
+                  <ul>
+                    {tagsList.map((tag) => (
+                      <li key={tag.unique_tag_id}>{tag.tag}</li>
+                    ))}
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
