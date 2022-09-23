b6de2ced13e304e7e156ae3498c8bd07f952c47c
Salman0112358 <Salman0112358@outlook.com>
2022-09-23 10:42:28 +0100

    bunch of random space pictures for thumbnail renders

---
+++ src/assets/images/spacePictures.ts
@@ -0,0 +1,45 @@
+import one from "./1.png";
+import two from "./2.png";
+import three from "./3.png";
+import four from "./4.png";
+import five from "./5.png";
+import six from "./6.png";
+import seven from "./7.png";
+import eight from "./8.png";
+import nine from "./9.png";
+import ten from "./10.png";
+import eleven from "./11.png";
+import twelve from "./12.png";
+import thirteen from "./13.png";
+import fourteen from "./14.png";
+import fifteen from "./15.png";
+import sixteen from "./16.png";
+import seventeen from "./17.png";
+import eightteen from "./18.png";
+import nineteen from "./19.png";
+import twenty from "./20.png";
+
+const spacePictures = {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  eleven,
+  twelve,
+  thirteen,
+  fourteen,
+  fifteen,
+  sixteen,
+  seventeen,
+  eightteen,
+  nineteen,
+  twenty,
+};
+
+export default spacePictures;
