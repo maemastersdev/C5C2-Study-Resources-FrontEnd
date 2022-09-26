40a2e3415e9a1dffb3bec75849bd100c4ca458b0
Salman0112358 <Salman0112358@outlook.com>
2022-09-26 20:55:46 +0100

    adding default export for modal component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Grid } from "./Grid/Grid";
 export { default as ResourceCard } from "./ResourceCard/ResourceCard";
 export { default as ResourceSubmissionForm } from "./ResourceSubmissionForm/ResourseSubmissionForm";
-export {default as BaseModel} from './BaseModal/BaseModal'
+export {default as BaseModal} from "./BaseModal/BaseModal"
