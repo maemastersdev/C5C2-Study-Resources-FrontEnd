fac5539a2d0be347b8a03347835d19b98e6ea599
Salman0112358 <Salman0112358@outlook.com>
2022-09-26 20:55:09 +0100

    Added Modal container to hold post submission form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 /*----------------Importing React Components-------------------------*/
-import { Grid, ResourceSubmissionForm } from "./components/index";
+
+import { Grid, ResourceSubmissionForm, BaseModal } from "./components/index";
 
 function App(): JSX.Element {
   return (
     <>
       <Grid />
-      <ResourceSubmissionForm user_id={"1"} user_name={"Neo"} />
+      <BaseModal/>
     </>
   );
 }
